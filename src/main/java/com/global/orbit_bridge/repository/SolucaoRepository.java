3a288aba12ad03b09771f58d56b54145ef0c75f7
Arthur Gomes <arthurszaa7@icloud.com>
2026-05-29 00:33:15 -0300

    new: novas funcoes de consulta

--- src/main/java/com/global/orbit_bridge/repository/SolucaoRepository.java
+++ src/main/java/com/global/orbit_bridge/repository/SolucaoRepository.java
@@ -4,6 +4,14 @@ import com.global.orbit_bridge.model.SolucaoEspacial;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface SolucaoRepository extends JpaRepository<SolucaoEspacial, Long> {
+    Optional<List<SolucaoEspacial>> getAllByAreaAtuacao(String areaAtuacao);
+
+    Optional<List<SolucaoEspacial>> getAllByOds(Integer ods);
+
+    Integer findAllByPrioridadeAlta();
 }
