033c9128c3eef2ee48adf6d582a7421127357c42
Arthur Gomes <arthurszaa7@icloud.com>
2026-05-29 00:33:37 -0300

    new: ajustes com Bean Validation

--- src/main/java/com/global/orbit_bridge/dto/OrganizacaoDto.java
+++ src/main/java/com/global/orbit_bridge/dto/OrganizacaoDto.java
@@ -1,12 +1,15 @@
 package com.global.orbit_bridge.dto;
 
 import com.global.orbit_bridge.model.SolucaoEspacial;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.Set;
 @Data
 public class OrganizacaoDto {
+    @NotBlank
     private String nome;
+    @NotBlank
     private String descricao;
     private Set<SolucaoEspacial> solucoes;
 }
--- src/main/java/com/global/orbit_bridge/dto/SolucaoDto.java
+++ src/main/java/com/global/orbit_bridge/dto/SolucaoDto.java
@@ -3,17 +3,21 @@ package com.global.orbit_bridge.dto;
 import com.global.orbit_bridge.model.Organizacao;
 import com.global.orbit_bridge.model.enums.Prioridade;
 import com.global.orbit_bridge.model.enums.StatusSolucao;
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SolucaoDto {
+    @NotBlank
     private String nome;
+    @NotBlank
     private String descricao;
+    @NotBlank
     private String areaAtuacao;
     private StatusSolucao status;
     private Prioridade prioridade;
-    private List<String> ods;
+    private List<Integer> ods;
     private Organizacao idOrganizacaoResponsavel;
 }
