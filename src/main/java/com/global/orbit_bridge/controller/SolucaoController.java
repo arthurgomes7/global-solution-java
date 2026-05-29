e1d49f912de47b587cbd5dfac45ed8e4022769d6
Arthur Gomes <arthurszaa7@icloud.com>
2026-05-29 00:34:03 -0300

    new: adicao de SolucaoController

---
+++ src/main/java/com/global/orbit_bridge/controller/SolucaoController.java
@@ -0,0 +1,10 @@
+package com.global.orbit_bridge.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/solucoes")
+public class SolucaoController {
+
+}
