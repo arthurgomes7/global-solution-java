82ca24789710e47511fd973f9dbb49ec70d0aadd
Arthur Gomes <arthurszaa7@icloud.com>
2026-06-02 20:30:38 -0300

    new: Criando Exception

---
+++ src/main/java/com/global/orbit_bridge/exceptions/SolucaoException.java
@@ -0,0 +1,7 @@
+package com.global.orbit_bridge.exceptions;
+
+public class SolucaoException extends RuntimeException{
+    public SolucaoException(String message) {
+        super(message);
+    }
+}
