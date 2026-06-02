471279d78097aa313d762cb1a16aeefd88193293
Arthur Gomes <arthurszaa7@icloud.com>
2026-06-02 00:22:14 -0300

    fix: Ajuste nos Dtos

--- src/main/java/com/global/orbit_bridge/dto/OrganizacaoDto.java
+++ src/main/java/com/global/orbit_bridge/dto/OrganizacaoDto.java
@@ -2,14 +2,15 @@ package com.global.orbit_bridge.dto;
 
 import com.global.orbit_bridge.model.SolucaoEspacial;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.Set;
 @Data
 public class OrganizacaoDto {
-    @NotBlank
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(max = 30, message = "O nome não pode ter mais do que 30 caracteres")
     private String nome;
-    @NotBlank
     private String descricao;
     private Set<SolucaoEspacial> solucoes;
 }
--- src/main/java/com/global/orbit_bridge/dto/SolucaoDto.java
+++ src/main/java/com/global/orbit_bridge/dto/SolucaoDto.java
@@ -4,17 +4,21 @@ import com.global.orbit_bridge.model.Organizacao;
 import com.global.orbit_bridge.model.enums.Prioridade;
 import com.global.orbit_bridge.model.enums.StatusSolucao;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
 public class SolucaoDto {
-    @NotBlank
+    @NotBlank(message = "O nome não pode estar em branco")
+    @Size(max = 30, message = "O nome não pode ter mais que 30 caracteres")
     private String nome;
-    @NotBlank
+    @NotBlank(message = "A descrição não pode ser vazia")
     private String descricao;
     @NotBlank
+    @Size(max = 30, message = "A área de atuação não pode ter mais que 30 caracteres")
     private String areaAtuacao;
     private StatusSolucao status;
     private Prioridade prioridade;
